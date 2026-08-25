5bdb3e1abc2597fa42d02a2c96bcc86ca4b83ea1
Erdem Keles <erdemkeles@Erdems-MacBook-Air.local>
2026-08-25 15:49:23 +0300

    feat: azure zero-trust baseline with tofu (nsg,keyvault,rbac)

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.prefix}"
+  location = var.location
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${var.prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "DenyAllInbound"
+    priority                   = 4096
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "kv" {
+  name                          = "kv-${var.prefix}-001"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = false
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = false
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+
+}   
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  type    = string
+  default = "swedencentral"
+}
+variable "prefix" {
+  type    = string
+  default = "sec-lab"
+}
